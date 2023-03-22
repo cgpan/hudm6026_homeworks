fe5c9f7ea0db9d508732f28ec8b5e08e48ac80f7
cgpan <grepan3280@gmail.com>
2023-03-22 09:37:21 -0400

    HW07 New Start

--- homework_08_after spring break/08 Activity.R
+++ homework_08_after spring break/08 Activity.R
@@ -16,10 +16,18 @@ lalonde %>% ggplot(aes(x = factor(treat), y = re78)) +
   geom_boxplot()
 lalonde %>% ggplot(aes(x = re78),) +
   geom_density(aes(color = factor(treat)))
+# from the graph you can see the curve moved to the right
+# median is robuts about the kurtosis or skeww
+# but the mean is not 
+
 
 # Means/vars by group
 by(data = lalonde$re78, INDICES = lalonde$treat, FUN = mean)
+# the question is again, is the difference significant?
 by(data = lalonde$re78, INDICES = lalonde$treat, FUN = var)
+
+
+
 library(car)
 leveneTest(re78 ~ factor(treat), data = lalonde)
 
@@ -30,13 +38,25 @@ summary(lm1)
 # Treatment effect is estimated at $886.30 and is not
 # significant (p = .061). 
 
-# Run the two-sample t-test with Welch's df.
+# Run the two-sample t-test with Welch's df. 
+# This does not assume the equal variance
 t.test(re78 ~ treat, data = lalonde, var.equal = FALSE)
-
-# Run Kolmogorov-Smirnov test.
+# -------------------------------------------------------
+# Run Kolmogorov-Smirnov test. Not sensitive to the change in the tail
+# it is sensitive to the change in the center of distribution
 ks.test(x = lalonde$re78[lalonde$treat == 0],
         y = lalonde$re78[lalonde$treat == 1])
 # Two-sided test is not significant (p = .091).
+
+# ---------------------------------------------------------------
+# we need to consider to control for the covariate
+# often the case is the priori outcome is the strongly correlated to
+# the outcome right now
+# ---------------------------------------------------------------
+
+# what is the good candidate is for the statistical test for the difference?
+
+
 
 # Were there baseline differences in earnings?
 by(data = lalonde$re75, INDICES = lalonde$treat, FUN = mean)
@@ -46,6 +66,11 @@ by(data = lalonde$re75, INDICES = lalonde$treat, FUN = mean)
 # to do that is via regression.
 lm2 <- lm(re78 ~ treat + re75, data = lalonde)
 summary(lm2)
+
+# ---------------------------------------------------------------
+# what is the good candidate is for the statistical test for the difference?
+
+
 
 # It looks like the treatment group distribution may have a 
 # heavier tail than the control group. 
@@ -60,6 +85,9 @@ kurt <- function(vec) {
   return(kt) }
 kurt(lalonde$re78[lalonde$treat == 1])
 kurt(lalonde$re78[lalonde$treat == 0])
+# treatment group's kurtosis is 3 times greater than the control group
+
+
 
 # One great strength of resampling methods is that we can 
 # use them to learn about the sampling distributions of 
@@ -78,7 +106,7 @@ kurt(lalonde$re78[lalonde$treat == 0])
 # named re78, and permutation indexes named ind, the function will
 # permute the outcome while holding the group labels fixed and then
 # calculate and return sample kurtosis on the permuted data.
-kurt_diff <- function(dat, ind) {
+kurt_diff <- function(dat, ind) { # ind = indices
   trt <- dat$treat # group assignments are fixed
   re78 <- dat$re78[ind] # permute the outcome
   k0 <- kurt(re78[which(trt == 0)])
@@ -86,15 +114,17 @@ kurt_diff <- function(dat, ind) {
   return(k1 - k0)
 }
 
+library(boot)
 set.seed(3764)
 boot_out <- boot(data = lalonde, 
-                 statistic = kurt_diff, 
+                 statistic = kurt_diff, #apply this function to each of the 
                  R = 1000, sim = "permutation")
 
 # The statistics resulting from permutation are stored in 
 # the output in a 1000 x 1 matrix called t.
 hist(boot_out$t, 50)
-
+# ------------------------------
+# we hope to improve , we do one side test here
 # Calculate ASL (approximate significance level)
 length(which(boot_out$t > boot_out$t0))/1000
 
@@ -136,8 +166,6 @@ hist(boot_out2$t, 50)
 # Calculate ASL (approximate significance level)
 length(which(boot_out2$t > boot_out2$t0))/1000
 
-
-
 # Your group work will involve the acupuncture data set. I've
 # uploaded a csv file in the Misc folder in the Files section.
 
@@ -168,17 +196,47 @@ length(which(boot_out2$t > boot_out2$t0))/1000
 df <- read.csv("acupuncture.csv")
 head(df)
 
+df$effect <- df$pk5 - df$pk1
+head(df)
+# subset the treatment and control group
+group_treat <- df$effect[which(df$group==1)]
+group_control <- df$effect[which(df$group==0)]
+
+# draw the graph of treatment and control group's effect
+library(tidyverse)
+df %>% ggplot(aes(x = factor(group), y = effect)) + 
+  geom_boxplot()
+df %>% ggplot(aes(x = effect),) +
+  geom_density(aes(color = factor(group)))
+
+# to test the equal variance 
+library(car)
+leveneTest(effect ~ factor(group), data = df)
+
+# the result shows that the equal variance hold!
+
+# run the standard two sample t-test
+t.test(group_treat, group_control,var.equal = T)
+t.test(group_treat, group_control,var.equal = F)
+
+# --------------- can run on pk5----------------
+# for the randomly experiments, you do not need to consider the 
+# difference. But you need to check about
+# check the cohen's d in the baseline, if the >.1,
 
 
 # Part 2 is to identify a non-standard test statistic that 
 # your team suspects will also be sensitive to departures 
 # from the null hypothesis of no treatment effect. 
-
+# leveene's test of the variance ratio 
+# or correlation pk1-pk5 in treatment/ pk1-pk5 in control
+# rank all the data in pk5, 
 
 
 # Part 3 is to use the non-standard test statistic in a 
 # permutation framework to determine the approximate significance
 # level (p-value).
+
 
 # The task for homework is to write up the results of 1, 2, and 3.
 
